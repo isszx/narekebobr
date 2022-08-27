13af03dd562a72b25ad3e687dd45d00eeb0ac792
isszx <mepfunc@gmail.com>
2022-08-27 20:35:53 +0300

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://narekebobr.by',
+	site: 'https://isszx.github.io', // 'https://narekebobr.by',
+	base: '/narekebobr',
 	integrations: [mdx(), sitemap()],
 });
